57a9eddae4fe2da60f88256eae603230da97f9d9
joey.scarr@gmail.com <joey.scarr@gmail.com@7c240197-1839-df0d-a9ee-3fd895d9b61c>
2011-04-26 03:48:53 +0000

    Use invoke for predefined effects when required

--- DotNetFX/ResizeWidth.cs
+++ DotNetFX/ResizeWidth.cs
@@ -41,7 +41,7 @@ namespace DotNetFX {
         /// <summary>
         /// Animation event handler that will resize an element by setting its width.
         /// </summary>
-        protected override void UpdateStyle() {
+        protected override void UpdateStyleInternal() {
             m_Control.Width = (int)Math.Round(m_Current[0]);
         }
     }
